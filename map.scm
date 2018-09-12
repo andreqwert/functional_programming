5173dc8dce6bd586cc9f236d8bc273eb7ec52e69
Andrey <timebird@yandex.ru>
2018-09-12 04:24:01 +0300

    Update map.scm

--- map.scm
+++ map.scm
@@ -1,6 +1,7 @@
 (define (my-map proc items)
   (if (null? items) '()
       (cons (proc (car items))
+            ; Передаём хвост списка рекурсивно функции на вход
             (my-map proc (cdr items)))))
 
 (my-map abs (list -1 -2 3 4)) ; (1 2 3 4)
