f5309610c2b0692fa625e88e285a6c30bf42c594
Andrey <timebird@yandex.ru>
2018-10-01 13:35:44 +0300

    Update trace.scm

--- 2/trace.scm
+++ 2/trace.scm
@@ -3,9 +3,18 @@
     ; выполняется вывод значения в консоль,
     ; выполняется перевод на новую строку,
     ; выполняется возврат списка в программу
-    ; ((_ xs) (and (display xs) (newline) xs))))
     ((_ xs) (and (write (quote xs))
               (display " => ")
               (write xs)
               (newline)
               xs))))
+
+
+; (car xss) => (1 2 3)
+; xss => ((1 2 3) (one two three))
+; (car xss) => (2 3)
+; xss => ((2 3) (two three))
+; (car xss) => (3)
+; xss => ((3) (three))
+; (car xss) => ()
+; ((1 one) (2 two) (3 three))
