562bc45ef9e189507f226c702f55859da1167996
Andrey <timebird@yandex.ru>
2018-10-01 13:34:14 +0300

    Update trace.scm

--- 2/trace.scm
+++ 2/trace.scm
@@ -3,4 +3,9 @@
     ; выполняется вывод значения в консоль,
     ; выполняется перевод на новую строку,
     ; выполняется возврат списка в программу
-    ((_ xs) (and (display xs) (newline) xs))))
+    ; ((_ xs) (and (display xs) (newline) xs))))
+    ((_ xs) (and (write (quote xs))
+              (display " => ")
+              (write xs)
+              (newline)
+              xs))))
